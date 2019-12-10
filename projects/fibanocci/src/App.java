a28f3f5452b9a0991d9810b8a6768374646e135d
Sherryl <smathe38@uic.edu>
2019-12-09 23:08:16 -0600

    corrected fibannocci

--- projects/fibanocci/src/App.java
+++ projects/fibanocci/src/App.java
@@ -1,28 +1,30 @@
-// Fibonacci Series using Dynamic Programming 
-class App 
-{ 
-   static void fib(int n) 
-    { 
-        int a = 0, b = 1, c; 
-        if (n == 0) {
-            System.out.println(a + " ");
-            return;
-        }
-        for (int i = 2; i <= n; i++) 
-        { 
-            c = a + b; 
-            a = b; 
-            b = c; 
-        } 
-        System.out.println(b+ " ");
-    } 
-  
-    public static void main (String args[]) 
-    { 
-        int n = 9; 
-	if(args.length > 0)
-	 n = Integer.parseInt(args[0]);
-        fib(n); 
-    }  
+// Fibonacci Series using Dynamic Programming
+class App
+{
+	// Method to print first n Fibonacci Numbers
+	static void printFibonacciNumbers(int n)
+	{
+		int f1 = 0, f2 = 1, i;
+
+		if (n < 1)
+			return;
+
+		for (i = 1; i <= n; i++)
+		{
+			System.out.print(f2+" ");
+			int next = f1 + f2;
+			f1 = f2;
+			f2 = next;
+		}
+	}
+
+	// Driver Code
+	public static void main(String[] args)
+	{
+		int n = 9;
+		if(args.length > 0)
+			n = Integer.parseInt(args[0]);
+		printFibonacciNumbers(n);
+	}
 } 
 
